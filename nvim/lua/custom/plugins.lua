ff498215613e298a42aa042b350a4e77f5fc81a1
Bhupesh Rawat <75159891+bhupesh7387@users.noreply.github.com>
2023-08-05 09:28:58 +0530

    Add files via upload

---
+++ nvim/lua/custom/plugins.lua
@@ -0,0 +1,23 @@
+local plugins ={
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        lazy = false,
+        config = function ()
+            require('custom.configs.lsp-zero')
+        end,
+        branch = 'v2.x',
+        dependencies = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'williamboman/mason.nvim'},           -- Optional
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
+        },
+    }
+}
+return plugins
+
